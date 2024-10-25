16d5f85215795482c84a01ec54a28d6feeb4b4cf
tatyana <sazonko.tatyana@gmail.com>
2024-10-25 22:48:55 +0300

    fead: write test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.currentNum = 0;   
+    }
+    
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        console.log(`Min - ${this.min}`, `Max - ${this.max}`)
+        this.currentNum = Math.ceil((this.min + this.max) / 2);
+        return this.currentNum;
     }
 
     lower() {
-
+        this.max = this.currentNum;
     }
 
     greater() {
-
+        this.min = this.currentNum;
     }
 }
 
